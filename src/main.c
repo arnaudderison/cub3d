a5d18f8f595dc86c48ce563fc87f599be2d76318
arnaudderison <arnaud.derison@hotmail.com>
2025-03-04 21:31:34 +0100

    test

--- src/main.c
+++ src/main.c
@@ -6,14 +6,27 @@
 /*   By: aderison <aderison@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 19:08:38 by aderison          #+#    #+#             */
-/*   Updated: 2025/03/04 19:18:49 by aderison         ###   ########.fr       */
+/*   Updated: 2025/03/04 21:31:28 by aderison         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "mlx.h"
+#include <stdlib.h>
 
-int main()
+int	main(void)
 {
-    ft_printf("CUB3D\n");
-    return 0;
+	void	*mlx;
+	void	*win;
+
+	// Initialiser MiniLibX
+	mlx = mlx_init();
+	if (!mlx)
+		return (1);
+	// Créer une fenêtre 500x500
+	win = mlx_new_window(mlx, 500, 500, "Test MiniLibX");
+	if (!win)
+		return (1);
+	// Afficher la fenêtre et attendre
+	mlx_loop(mlx);
+	return (0);
 }
