4b23426b2d1c031fc2f4d4c240da0685128f53c1
arnaudderison <arnaud.derison@hotmail.com>
2025-03-20 15:03:53 +0100

    no leaks if failed init image

--- src/init/init_img.c
+++ src/init/init_img.c
@@ -6,7 +6,7 @@
 /*   By: aderison <aderison@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 20:26:36 by aderison          #+#    #+#             */
-/*   Updated: 2025/03/20 13:31:56 by aderison         ###   ########.fr       */
+/*   Updated: 2025/03/20 15:03:16 by aderison         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	create_img_from_xpm(t_cub3d *cub3d, t_img *img, char *path)
 {
 	t_win	win;
 
-	int size; // taille d'un xpm pourrait changer a voir avec pauline !
+	int size;
 	*img = (t_img){0};
 	win = cub3d->win;
 	size = TEX_SIZE;
@@ -31,8 +31,8 @@ void	init_img(t_cub3d *cub3d, t_img *image, int width, int height)
 {
 	*image = (t_img){0};
 	image->img = mlx_new_image(cub3d->win.mlx, width, height);
-	// if (image->img == NULL)
-	// free ? et exit
+	if (image->img == NULL)
+		error(UNKNOWN, cub3d, &freeall);
 	image->addr = (int *)mlx_get_data_addr(image->img, &image->pixel_bits,
 			&image->size_line, &image->endian);
 	return ;
